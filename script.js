b3bfd675f13498135e0ab1de3f849cf400b7c484
Gradnos <sdolaqidze@gmail.com>
2023-02-12 12:35:00 +0400

    Create playRound function

--- script.js
+++ script.js
@@ -3,15 +3,38 @@ function getComputerChoice(){
     let choice = "none"
     switch (caseNumber){
         case 1 : 
-            choice = "Rock";
+            choice = "rock";
             break;
         case 2 :
-            choice = "Paper";
+            choice = "paper";
             break;
         case 3 :
-            choice = "Scissors";
+            choice = "scissors";
             break;
     }
 
     return choice;
 }
+
+function playRound(playerSelection, computerSelection){
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = computerSelection.toLowerCase();
+    let roundResult = "none";
+    if (computerSelection === playerSelection)  roundResult = "Draw, both players chose same";
+    else if (playerSelection === "rock") {
+        if (computerSelection === "paper") roundResult = "Computer wins, paper beats rock";
+        if (computerSelection === "scissors") roundResult = "Player wins, rock beats scissors";
+    }
+    else if (playerSelection === "paper") {
+        if (computerSelection === "rock") roundResult = "Player wins, paper beats rock";
+        if (computerSelection === "scissors") roundResult = "Computer wins, scissors beat paper";
+    }
+    else if (playerSelection === "scissors") {
+        if (computerSelection === "rock") roundResult = "Computer wins, rock beats scissors";
+        if (computerSelection === "paper") roundResult = "Player wins, scissors beat paper";
+    }
+    console.log(computerSelection);
+    return(roundResult);
+}
+
+alert(playRound(prompt("rock paper scissors"), getComputerChoice()));;
