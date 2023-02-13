17697eac7080336837efa055024ee9a51afdd20d
Gradnos <sdolaqidze@gmail.com>
2023-02-13 10:07:59 +0400

    Create game and displayData functions

--- script.js
+++ script.js
@@ -33,21 +33,49 @@ function playRound(playerSelection, computerSelection){
         if (computerSelection === "rock") roundData = ["Computer wins, rock beats scissors", "computer"];
         if (computerSelection === "paper") roundData = ["Player wins, scissors beat paper", "player"];
     }
-    console.log(`${playerSelection} / ${computerSelection} / ${roundData} \n`);
+    console.log(`Your Selection : ${playerSelection} / Computers Selection : ${computerSelection}`);
     return(roundData);
 }
 
 function game()
 {
+    let playerScore = 0, computerScore = 0;
+    let playerSelection, computerSelection;
     for (let i = 0; i < 5; i++){
-
+        playerSelection = prompt("What do you play \n Rock , Paper, Scissors");
+        computerSelection = getComputerChoice();
+        roundData = playRound(playerSelection,computerSelection);
+        if(roundData[1] === "computer"){
+            computerScore++;
+            displayData(roundData[0], playerScore, computerScore);
+        }
+        if(roundData[1] === "player"){
+            playerScore++;
+            displayData(roundData[0], playerScore, computerScore);
+        }
+        if(roundData[1] === "draw"){
+            displayData(roundData[0], playerScore, computerScore);
+        }
     }
+
+    if(playerScore > computerScore) console.log("Player Won!");
+    if(playerScore < computerScore) console.log("Computer Won!");
+    if(playerScore === computerScore) console.log("Draw!");
+
+
 }
+
+function displayData(roundMessage, playerScore, computerScore){
+    console.log(`${roundMessage} \n Player Score : ${playerScore} /// Computer Score : ${computerScore}`);
+}
+
+
+game();
 
 // 1000 computer vs computer games
-for (i = 0; i < 10; i++){
-    playRound(getComputerChoice(), getComputerChoice());
-}
+// for (i = 0; i < 10; i++){
+//     playRound(getComputerChoice(), getComputerChoice());
+// }
 
 
 //player vs computer
